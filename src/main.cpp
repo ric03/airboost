51ff341d6df5fc78196629eb329f0050fc89c63f
Tom Schneider <ric0.github@gmail.com>
2023-10-05 22:46:40 +0200

    refactor: extract functions into sdc30.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,150 +1,21 @@
 #include <Arduino.h>
-#include <Adafruit_SCD30.h>
-#include "scan.h"
+#include "scd30.h"
 
-Adafruit_SCD30 scd30;
 // put function declarations here:
 
 void setup()
 {
-  // setupI2C();
-  // loopI2C();
 
   Serial.begin(115200);
+  setupSDC30();
 
   while (!Serial)
     delay(10); // will pause until serial console opens, use the RESET button to start monitoring
-
-  Serial.println("Adafruit SCD30 Sensor adjustment test!");
-  Serial.println("Attempting to find SCD30 chip (CO2-Sensor)");
-
-  // TwoWire i2cCo2Sensor = TwoWire(0);
-  // i2cCo2Sensor.begin(21, 22);
-
-  // if (!scd30.begin(SCD30_I2CADDR_DEFAULT, &i2cCo2Sensor))
-  if (!scd30.begin())
-  {
-    Serial.println("Failed to find SCD30 chip");
-    while (1)
-    {
-      delay(10);
-    }
-  }
-  Serial.println("SCD30 Found!");
-
-  /***
-   * The code below will report the current settings for each of the
-   * settings that can be changed. To see how they work, uncomment the setting
-   * code above a status message and adjust the value
-   *
-   * **Note:** Since Automatic self calibration and forcing recalibration with
-   * a reference value overwrite each other, you should only set one or the other
-   ***/
-
-  /*** Adjust the rate at which measurements are taken, from 2-1800 seconds */
-  // if (!scd30.setMeasurementInterval(5)) {
-  //   Serial.println("Failed to set measurement interval");
-  //   while(1){ delay(10);}
-  // }
-  Serial.print("Measurement interval: ");
-  Serial.print(scd30.getMeasurementInterval());
-  Serial.println(" seconds");
-
-  /*** Restart continuous measurement with a pressure offset from 700 to 1400 millibar.
-   * Giving no argument or setting the offset to 0 will disable offset correction
-   */
-  // if (!scd30.startContinuousMeasurement(15)){
-  //   Serial.println("Failed to set ambient pressure offset");
-  //   while(1){ delay(10);}
-  // }
-  Serial.print("Ambient pressure offset: ");
-  Serial.print(scd30.getAmbientPressureOffset());
-  Serial.println(" mBar");
-
-  /*** Set an altitude offset in meters above sea level.
-   * Offset value stored in non-volatile memory of SCD30.
-   * Setting an altitude offset will override any pressure offset.
-   */
-  // if (!scd30.setAltitudeOffset(110)){
-  //   Serial.println("Failed to set altitude offset");
-  //   while(1){ delay(10);}
-  // }
-  Serial.print("Altitude offset: ");
-  Serial.print(scd30.getAltitudeOffset());
-  Serial.println(" meters");
-
-  /*** Set a temperature offset in hundredths of a degree celcius.
-   * Offset value stored in non-volatile memory of SCD30.
-   */
-  // if (!scd30.setTemperatureOffset(1984)){ // 19.84 degrees celcius
-  //   Serial.println("Failed to set temperature offset");
-  //   while(1){ delay(10);}
-  // }
-  Serial.print("Temperature offset: ");
-  Serial.print((float)scd30.getTemperatureOffset() / 100.0);
-  Serial.println(" degrees C");
-
-  /*** Force the sensor to recalibrate with the given reference value
-   * from 400-2000 ppm. Writing a recalibration reference will overwrite
-   * any previous self calibration values.
-   * Reference value stored in non-volatile memory of SCD30.
-   */
-  // if (!scd30.forceRecalibrationWithReference(400)){
-  //   Serial.println("Failed to force recalibration with reference");
-  //   while(1) { delay(10); }
-  // }
-  Serial.print("Forced Recalibration reference: ");
-  Serial.print(scd30.getForcedCalibrationReference());
-  Serial.println(" ppm");
-
-  /*** Enable or disable automatic self calibration (ASC).
-   * Parameter stored in non-volatile memory of SCD30.
-   * Enabling self calibration will override any previously set
-   * forced calibration value.
-   * ASC needs continuous operation with at least 1 hour
-   * 400ppm CO2 concentration daily.
-   */
-  // if (!scd30.selfCalibrationEnabled(true)){
-  //   Serial.println("Failed to enable or disable self calibration");
-  //   while(1) { delay(10); }
-  // }
-  if (scd30.selfCalibrationEnabled())
-  {
-    Serial.print("Self calibration enabled");
-  }
-  else
-  {
-    Serial.print("Self calibration disabled");
-  }
-
-  Serial.println("\n\n");
 }
 
 void loop()
 {
-  if (scd30.dataReady())
-  {
-
-    if (!scd30.read())
-    {
-      Serial.println("Error reading sensor data");
-      return;
-    }
-
-    Serial.print("Temperature: ");
-    Serial.print(scd30.temperature);
-    Serial.println(" degrees C");
-
-    Serial.print("Relative Humidity: ");
-    Serial.print(scd30.relative_humidity);
-    Serial.println(" %");
-
-    Serial.print("CO2: ");
-    Serial.print(scd30.CO2, 3);
-    Serial.println(" ppm");
-    Serial.println("");
-  }
-
+  readScd30Data();
   delay(100);
 }
 
