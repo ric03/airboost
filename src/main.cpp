22bd92e071e36a0a5c03e163a89c4bf0b957b9a4
Tom Schneider <ric0.github@gmail.com>
2023-10-06 23:17:21 +0200

    feat: remove feature toggle to enable scd30
    unecessary config, there is no  point in having it

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include "scd30.h"
 
-#define SCD30_ENABLED true
 #define LOOP_DEPLAY 250
 
 void setup()
@@ -10,20 +9,16 @@ void setup()
   while (!Serial)
     delay(10); // will pause until serial console opens, use the RESET button to start monitoring
 
-#if SCD30_ENABLED == true
   scd30::setup();
-#endif
 }
 
 void loop()
 {
-#if SCD30_ENABLED == true
   scd30::Data *data = scd30::readData();
   if (data)
   {
     scd30::printSerial(data);
   }
-#endif
 
   delay(LOOP_DEPLAY);
 }
